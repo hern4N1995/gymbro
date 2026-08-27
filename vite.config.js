ab83f559cbc540e86559b1751bf0e78b92254e90
Hernán Alegre <hernan.cs@hotmail.com>
2026-08-27 01:41:57 -0300

    prueba

--- vite.config.js
+++ vite.config.js
@@ -19,9 +19,9 @@ export default defineConfig({
         scope: '/',
         icons: [
           {
-            src: 'icon.svg',
-            sizes: 'any',
-            type: 'image/svg+xml',
+            src: 'icon.png',
+            sizes: '512x512',
+            type: 'image/png',
             purpose: 'any maskable'
           },
           {
